ed0001c1b42a6697a8ac433b1a42eaaa4a9a804a
anaangelicaf <86435558+anaangelicaf@users.noreply.github.com>
2021-10-21 21:30:27 -0300

    cambio develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+//ana
 function App() {
   return (
     <div className="App">
+      <p> Ana</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"

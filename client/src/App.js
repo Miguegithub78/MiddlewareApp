54f06aafd829ed90b5d02dc0722609af7468c584
Ailin <ailintango@gmail.com>
2021-10-26 12:30:17 -0300

    LandingPage

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/login";
 import Home from "./components/Home";
+import LandingPage from './components/LandingPage';
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={Login} />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/" component={LandingPage} />
         {/* <RutaPrivada exact path="/proyectos" component={Proyectos} /> */}
       </Switch>
     </Router>

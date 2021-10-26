3d5e80a11a825604ed220f9b4a092841a3025118
anaangelicaf <86435558+anaangelicaf@users.noreply.github.com>
2021-10-26 12:50:38 -0300

    prueba search

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/login";
-import Home from "./components/Home";
+//import Login from "./components/login";
+import Home from "./components/Home.jsx";
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/login" component={Login} />
+        {/*<Route exact path="/login" component={Login} />*/}
         <Route exact path="/" component={Home} />
         {/* <RutaPrivada exact path="/proyectos" component={Proyectos} /> */}
       </Switch>

9fc256707c620297efae89a46c5737c8c150602d
Ailin <ailintango@gmail.com>
2021-10-26 12:37:16 -0300

    se agrega navBar

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/login";
 import Home from "./components/Home";
 import LandingPage from './components/LandingPage';
+import NavBar from "./components/NavBar/NavBar"
 function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/home" component={Home} />

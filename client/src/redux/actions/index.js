cd37d3d938a5392425e0b7f967a4fe860f7ff51e
Eva <mac@macs-MacBook-Pro.local>
2021-10-27 13:33:54 -0300

    reparando pull

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,5 +1,5 @@
 import { LOGIN_OKEY, LOGOUT_OKEY, LOGIN_GOOGLE, LOGIN_GUITHUB } from "../types";
-import axios from "axios";
+
 import clienteAxios from "../../components/config/clienteAxios";
 
 import { auth } from "../../firebaseConfig";
@@ -18,9 +18,9 @@ export const loginUserAction = (provider) => {
     try {
       if (provider === "google") {
         await signInWithPopup(auth, googleProvider).then((user) =>
-        await clienteAxios.post('/')
-        );
+      
         dispatch(loginOkey(user))
+        );
       } else if (provider === "guithub") {
         await signInWithPopup(auth, guithubProvider).then((user) =>
           dispatch(loginOkey(user))

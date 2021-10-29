bf1cbc9cd9e0e8c73ad6417ea2ee505f93e81946
sritapam <85538485+sritapam@users.noreply.github.com>
2021-10-29 18:01:03 -0300

    Update index.js

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -215,9 +215,9 @@ export function postPublications(payload) {
 	};
 }
 
-export function putPublications(id,data) {
+export function putPublications(id) {
 	return async function () {
-		const response = await clienteAxios.put(`/publications/${id}`),data;
+		const response = await clienteAxios.put(`/publications/${id}`);
 		return response;
 	};
 }

09ed3047e9e7462f28b92d1a2cf6a035b04eb892
Ailin <ailintango@gmail.com>
2021-10-30 02:29:45 -0300

    cambios miles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -19,13 +19,13 @@ function NavBar() {
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto ms-5 mb-2 mb-lg-0">
+					<ul className="navbar-nav mx-auto ">
 						{user && user.type === "junior" ? (
-							<li className="nav-item">
+							<li className="nav-item p-2 ">
 								<a className="nav-link" aria-current="page" href="#">Puente de los sueños</a>
 							</li>
 						) : null}
-						<li className="nav-item">
+						<li className="nav-item active">
 							<a className="nav-link fw-normal " aria-current="page" href="#">Empleos</a>
 						</li>
 						<li className="nav-item">

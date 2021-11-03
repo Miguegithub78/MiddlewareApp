383655e9cc1ad6989b650b214d8d55884af98ddf
Eva <mac@macs-MacBook-Pro.local>
2021-11-03 12:31:06 -0300

    reparando

--- client/src/components/ProfileUser/Languages.js
+++ client/src/components/ProfileUser/Languages.js
@@ -4,7 +4,6 @@ const Languages = ({ setInfoUser, infoUser }) => {
   const { languages } = useSelector((state) => state);
 
   const handleSelectLanguages = (lang) => {
-<<<<<<< HEAD
     setInfoUser((r) => {
       if (!r.languages.length === 0)
         return { ...r, infoUserChanged: true, languages: [lang] };
@@ -17,16 +16,6 @@ const Languages = ({ setInfoUser, infoUser }) => {
       } else {
         const filter = r.languages.filter((c) => c._id !== lang._id);
         return { ...r, infoUserChanged: true, languages: filter };
-=======
-
-    setInfoUser((r) => {
-      if (!r.languages.length === 0) return { ...r, languages: [lang] };
-      if (!r.languages.includes(lang)) {
-        return { ...r, languages: [...r.languages, lang] };
-      } else {
-        const filter = r.languages.filter((c) => c._id !== lang._id);
-        return { ...r, languages: filter };
->>>>>>> main
       }
     });
   };
@@ -37,7 +26,6 @@ const Languages = ({ setInfoUser, infoUser }) => {
         // console.log(infoUser.languages, lang , '//');
         // console.log(infoUser.languages.find(e=>e._id===lang._id)&&'hola');
         // console.log(infoUser.languages.name===lang.name);
-<<<<<<< HEAD
         return (
           <span key={i}>
             <input
@@ -62,27 +50,6 @@ const Languages = ({ setInfoUser, infoUser }) => {
           </span>
         );
       })}
-=======
-      return (
-        <span key={i}>
-          <input
-            style={{ focus: "none" }}
-            type="checkbox"
-            className="btn-check btn-checkbox-focus"
-            id={lang._id}
-            checked={infoUser.languages.find(e=>e._id===lang._id)?true:false}
-          />
-          <label
-            className="btn btn-outline-dark m-1 btn-checkbox-focus"
-            htmlFor={lang._id}
-            style={{ padding: "1px 5px" }}
-            onClick={() => handleSelectLanguages(lang)}
-          >
-            {lang.name}
-          </label>
-        </span>
-      )})}
->>>>>>> main
     </>
   )
 };

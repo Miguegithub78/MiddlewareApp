47f8073a2d195eeda5f88092f275fcb4130f251f
Pamela <85538485+sritapam@users.noreply.github.com>
2021-11-10 11:20:52 -0300

    Employ changes (#157)
    * Se agregan cambios a las rutas
    
    * Se descomenta las rutas
    
    * Se agregan datos a company
    
    * Se modifica carpeta nodemailer
    
    * Se agrega letter en job

--- api/src/models/collections/company.js
+++ api/src/models/collections/company.js
@@ -51,7 +51,11 @@ const companySchema = new Schema({
         type: String,
         required: false
     },
-    
+    address: {
+        type: String,
+        required: false
+    },
+                                 
     premium: {
         type: Boolean,
         default: false
@@ -61,6 +65,14 @@ const companySchema = new Schema({
         type: String,
         required: false,
         maxLength: 500
+    },
+    
+    latitude: {
+        type: String,
+    },
+
+    longitude: {
+        type: String,
     },
 
     technologies: [{

cbf5f0fce0d83dfb36f034debfb0b80450b5e092
Maximiliano Gadea <85596286+maxigadea@users.noreply.github.com>
2021-11-17 11:01:32 -0300

    primeros cambios

--- client/src/components/Notifications/Notifications.jsx
+++ client/src/components/Notifications/Notifications.jsx
@@ -4,20 +4,35 @@ import { onAuthStateChanged } from "firebase/auth";
 import {useHistory} from 'react-router-dom'
 import { auth } from "../../firebaseConfig";
 import { getUserAction } from '../../redux/actions';
+import Socket from "../socket.js"
+import { map } from '@firebase/util';
 
 
 const Notifications = () => {
     const user = useSelector((state) => state.user);
+    var [idUser, setIdUser] = useState(null);
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const [notifications, setNotifations] = useState([
+    const [notifications, setNotifications] = useState([]);
 
-    ]);
+    useEffect(()=>{
 
-    useEffect(() => {
-        console.log('prueba')
-    }, [])
+      setTimeout(()=>{
+  
+      setIdUser(user?._id)
+    }, 500)
+      
+  }, [user])
+
+    useEffect(()=>{
+
+    Socket.on('liked',(data)=>{
+        if(data.userPublicationId === idUser){
+        setNotifications([...notifications, data])
+      }
+     })
+  }, [Socket, idUser])
 
 onAuthStateChanged(auth, (userFirebase) => {
     if (userFirebase) {
@@ -29,11 +44,15 @@ onAuthStateChanged(auth, (userFirebase) => {
   });
 
 
-    return (
+    return notifications? (
         <div>
-           hola 
+           {notifications?.map(el => {
+             return (
+               <h1>{el.user + 'le dió me gusta a tu publicacion' }</h1>
+             )
+           })}
         </div>
-    )
+    ) : (<h1>No hay nada que mostrar</h1>)
 }
 
 export default Notifications;

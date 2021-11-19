4fdccef05c8d76a260ab298eae32891998796adb
Diego <diegoabad.2289@gmail.com>
2021-11-19 14:10:06 -0300

    Errores

--- api/src/routes/controllers/mercadopago.js
+++ api/src/routes/controllers/mercadopago.js
@@ -10,69 +10,69 @@ mercadopago.configure({
 });
 
 const create_preference = async (req, res) => {
-  // try{
-  const { idJob } = req.params;
-  const { plan } = req.query;
-  let product;
+	// try{
+	const { idJob } = req.params;
+	const { plan } = req.query;
+	let product;
 
-  if (plan === "premium") {
-    // const order = await Order.findOne({ _id: idJob });
-    // const { price, user } = order;
-    product = [
-      {
-        title: "Premium",
-        quantity: 1,
-        unit_price: 900,
-      },
-    ];
-  } else {
-    product = [
-      {
-        title: "Standard",
-        quantity: 1,
-        unit_price: 600,
-      },
-    ];
-  }
+	if (plan === 'premium') {
+		// const order = await Order.findOne({ _id: idJob });
+		// const { price, user } = order;
+		product = [
+			{
+				title: 'Premium',
+				quantity: 1,
+				unit_price: 900,
+			},
+		];
+	} else {
+		product = [
+			{
+				title: 'Standard',
+				quantity: 1,
+				unit_price: 600,
+			},
+		];
+	}
 
-  let preference = {
-    "items": "product",
-    "external_reference": `${idJob}/${product[0].title}`,
-    "notification_url": "https://hookb.in/VGLVqnXx0qHDrgoorlzJ",
-    "payment_methods": {
-      "excluded_payment_types": [
-        {
-          "id": "ticket",
-        },
-      ],
-      "excluded_payment_methods": [
-        {
-          "id": "atm",
-        },
-      ],
-      "installments": 1, //cant de cuotas
-      "default_payment_method_id": "visa",
-      "default_installments": 1,
-    },
-    "back_urls": {
-      "success": "https://middlewareapp-new.herokuapp.com/feedback",
-      "failure": "https://middlewareapp-new.herokuapp.com/feedback",
-      "pending": "https://middlewareapp-new.herokuapp.com/feedback",
-    },
-    "auto_return": "approved",
-  };
+	let preference = {
+		items: product,
+		external_reference: `${idJob}/${product[0].title}`,
+		notification_url: 'https://hookb.in/VGLVqnXx0qHDrgoorlzJ',
+		payment_methods: {
+			excluded_payment_types: [
+				{
+					id: 'ticket',
+				},
+			],
+			excluded_payment_methods: [
+				{
+					id: 'atm',
+				},
+			],
+			installments: 1, //cant de cuotas
+			default_payment_method_id: 'visa',
+			default_installments: 1,
+		},
+		back_urls: {
+			success: 'https://middlewareapp-new.herokuapp.com/feedback',
+			failure: 'https://middlewareapp-new.herokuapp.com/feedback',
+			pending: 'https://middlewareapp-new.herokuapp.com/feedback',
+		},
+		auto_return: 'approved',
+	};
 
-  mercadopago.preferences
-    .create(preference)
-    .then(function (response) {
-      console.info("Se crea preferencia");
-      console.log(response, "respuesta");
-      // console.log(response.body);
-      res.send({ id: response.body.id, product: product[0].title });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+	mercadopago.preferences
+		.create(preference)
+		.then(function (response) {
+			console.info('Se crea preferencia');
+			console.log(response, 'respuesta');
+			// console.log(response.body);
+			res.send({ id: response.body.id, product: product[0].title });
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
 
 	// } catch (error) {
 	//     res.status(404).json({ error: error.message });
@@ -80,9 +80,14 @@ const create_preference = async (req, res) => {
 };
 
 const orderFeedback = async (req, res) => {
-<<<<<<< HEAD
 	try {
-		const { collection_status, external_reference } = req.query;
+		const {
+			payment_id,
+			collection_status,
+			merchant_order_id,
+			external_reference,
+			status,
+		} = req.query;
 
 		const reference = external_reference.split('/');
 		const idJob = reference[0];
@@ -97,41 +102,7 @@ const orderFeedback = async (req, res) => {
 			{ new: true }
 		);
 
-		// res.json({
-		//   payment_id: req.query.payment_id,
-		//   payment_status: req.query.payment_status,
-		//const statusPayment = payment_status,
-		//   merchant_order_id: req.query.merchant_order_id,
-		//   date_created: req.query.date_created,
-		// });
-
-		// const companyPremium = Company.findOne({ _id : req.query.external_reference })
 		const jobData = await Jobs.findOne({ _id: idJob }).populate({
-=======
-  try {
-    const {
-      payment_id,
-      collection_status,
-      merchant_order_id,
-      external_reference,
-      status,
-    } = req.query;
-
-    const reference = external_reference.split("/");
-    const idJob = reference[0];
-    const plan = reference[1];
-    const level = plan === "Premium" ? 2 : 1;
-
-    const job = await Jobs.findOneAndUpdate(
-      { _id: idJob },
-      {
-        premium: level,
-      },
-      { new: true }
-    );
-
-    const jobData = await Jobs.findOne({ _id: idJob }).populate({
->>>>>>> main
 			path: 'company',
 		});
 		const gmailCompany = jobData.company.gmail;
